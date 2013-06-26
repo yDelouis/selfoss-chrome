074a5e147d82c51168da88b7460ac1c97490935e
Yoann Delouis <ydelouis@gmail.com>
2013-06-26 10:30:31 +0200

    Revert "Remove username and password from options"
    This reverts commit 499f8c92f9b172e3159b57179b0f1d77b24c7eda.

--- options.js
+++ options.js
@@ -2,7 +2,12 @@
 // Saves options to localStorage.
 function save_options() {
 	var selfossUrl = document.getElementById("url").value;
+	var username = document.getElementById("username").value;
+	var password = document.getElementById("password").value;
+
 	localStorage.selfossUrl = selfossUrl;
+	localStorage.username = username;
+	localStorage.password = password;
 
 	// Update status to let user know options were saved.
 	var status = document.getElementById("status");
@@ -15,9 +20,17 @@ function save_options() {
 // Restores select box state to saved value from localStorage.
 function restore_options() {
     var urlInput = document.getElementById("url");          
+	var usernameInput = document.getElementById("username");
+	var passwordInput = document.getElementById("password");
 	
 	if (localStorage.selfossUrl) {
 		urlInput.value = localStorage.selfossUrl;
+	}
+	if (localStorage.username) {
+		usernameInput.value = localStorage.username;
+	}
+	if (localStorage.password) {
+		passwordInput.value = localStorage.password;
 	}
 }
 
